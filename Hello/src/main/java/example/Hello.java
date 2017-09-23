a9c3ebaf93f36354ce74dae0e525ed76fe0ac7ab
vulchivivek <vvulchi@gmu.edu>
2017-09-22 17:30:01 -0700

    Second Commit

---
+++ Hello/src/main/java/example/Hello.java
@@ -0,0 +1,13 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<HelloRequest, HelloResponse> {
+
+    @Override
+    public HelloResponse handleRequest(HelloRequest input, Context context) {
+        return new HelloResponse(input.getInput());
+    }
+
+}
